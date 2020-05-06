1d30a183de10709053a7921fe3535948e760e336
= <dennisvandelaar@outlook.com>
2020-05-06 15:08:21 +0200

    mixed updates

--- User.sql
+++ User.sql
@@ -1,4 +1,4 @@
-CREATE USER [<identity-name>] FROM EXTERNAL PROVIDER;
+CREATE USER MyLogicAppDemo FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER MyLogicAppDemo;
 ALTER ROLE db_datawriter ADD MEMBER MyLogicAppDemo;
 GO
